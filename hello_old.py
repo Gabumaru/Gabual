ec1f6b1498c1ab5c241fa685e669ae874ed111d2
Gabumaru <gabu.ninefive@outlook.com>
2025-02-05 23:39:02 +0000

    First commit

--- hello_old.py
+++ hello_old.py
@@ -4,27 +4,23 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_mail import Mail, Message
-
+from datetime import datetime
 import requests
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.zoho.com'
-
-
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
@@ -58,37 +54,28 @@ class User(db.Model):
         return '<User %r>' % self.username
 
 
-def send_async_email(app, msg):
-    with app.app_context():
-        mail.send(msg)
+# Novo modelo Ocorrencia
+class Ocorrencia(db.Model):
+    __tablename__ = 'ocorrencias'
+    id = db.Column(db.Integer, primary_key=True)
+    descricao = db.Column(db.String(255), nullable=False)
+    data_ocorrencia = db.Column(db.DateTime, nullable=False)
+    disciplina = db.Column(db.String(64), nullable=False)
+
+    def __repr__(self):
+        return f'<Ocorrencia {self.id} - {self.descricao}>'
 
 
-def send_email(to, subject, template, **kwargs):
-    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
-                  sender=app.config['FLASKY_MAIL_SENDER'], recipients=[to])    
-    msg.html = render_template(template + '.html', **kwargs)
-    thr = Thread(target=send_async_email, args=[app, msg])
-    thr.start()
-    return thr
-
-def send_simple_message():
-  	return requests.post(
-  		"https://api.mailgun.net/v3/sandboxac1a400f6f814141b8b87a5c3d287cc5.mailgun.org/messages",
-  		auth=("api", "YOUR_API_KEY"),
-  		data={"from": "Excited User <mailgun@sandboxac1a400f6f814141b8b87a5c3d287cc5.mailgun.org>",
-  			"to": ["bar@example.com", "YOU@sandboxac1a400f6f814141b8b87a5c3d287cc5.mailgun.org"],
-  			"subject": "Hello",
-  			"text": "Testing some Mailgun awesomeness!"})
-
-
-class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+# Formulário para cadastrar ocorrências
+class OcorrenciaForm(FlaskForm):
+    descricao = TextAreaField('Descrição', validators=[DataRequired()])
+    disciplina = StringField('Disciplina', validators=[DataRequired()])
+    submit = SubmitField('Cadastrar')
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return dict(db=db, User=User, Role=Role)
+    return dict(db=db, User=User, Role=Role, Ocorrencia=Ocorrencia)
 
 
 @app.errorhandler(404)
@@ -101,22 +88,32 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    form = NameForm()
+# Alterado para 'não_disponivel' (nova página de cadastro)
+@app.route('/nao_disponivel', methods=['GET', 'POST'])
+def nao_disponivel():
+    form = OcorrenciaForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.name.data).first()
-        if user is None:
-            user = User(username=form.name.data)
-            db.session.add(user)
-            db.session.commit()
-            session['known'] = False
-            if app.config['FLASKY_ADMIN']:
-                send_email(app.config['FLASKY_ADMIN'], 'New User',
-                           'mail/new_user', user=user)
-        else:
-            session['known'] = True
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),
-                           known=session.get('known', False))
+        ocorrencia = Ocorrencia(
+            descricao=form.descricao.data,
+            disciplina=form.disciplina.data,
+            data_ocorrencia=datetime.utcnow()  # Grava a data atual
+        )
+        db.session.add(ocorrencia)
+        db.session.commit()
+        return redirect(url_for('nao_disponivel'))  # Redireciona após o cadastro
+
+    # Pega todas as ocorrências cadastradas e ordena por data
+    ocorrencias = Ocorrencia.query.order_by(Ocorrencia.data_ocorrencia.desc()).all()
+    
+    return render_template('não_disponivel.html', form=form, ocorrencias=ocorrencias)
+
+
+# Rota para visualizar todas as ocorrências
+@app.route('/ocorrencias', methods=['GET'])
+def listar_ocorrencias():
+    ocorrencias = Ocorrencia.query.order_by(Ocorrencia.data_ocorrencia.desc()).all()
+    return render_template('ocorrencias.html', ocorrencias=ocorrencias)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
